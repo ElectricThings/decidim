135c8b3de7ade6ad8f578024b4c24d94d3eb48bf
Jens Kraemer <jk@jkraemer.net>
2018-01-29 08:47:11 +0800

    only show active processes on home page

--- decidim-participatory_processes/app/queries/decidim/participatory_processes/highlighted_participatory_processes.rb
+++ decidim-participatory_processes/app/queries/decidim/participatory_processes/highlighted_participatory_processes.rb
@@ -5,7 +5,7 @@ module Decidim
     # This query selects some prioritized processes.
     class HighlightedParticipatoryProcesses < Rectify::Query
       def query
-        PrioritizedParticipatoryProcesses.new.query.limit(8)
+        PrioritizedParticipatoryProcesses.new.query.active.limit(8)
       end
     end
   end
